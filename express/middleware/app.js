687968a5ffb4f00845f7ca458d3c60134b3ce0ef
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-03-02 03:32:14 +0530

    routing use

--- express/middleware/app.js
+++ express/middleware/app.js
@@ -6,20 +6,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+const adminRoute = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
-});
-
-app.use('/product', (req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/', (req, res, next)=>{
-    res.send("<h1>Hello from Express</h1>");
-});
+app.use(adminRoute);
+app.use(shopRoutes);
 
 
 app.listen(3000);
