b3f860f18dc3ba387bdcc2606adb73abc046bc4b
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-03-10 01:50:12 +0530

    association

--- Express_Work_Project/app.js
+++ Express_Work_Project/app.js
@@ -10,17 +10,45 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user = user;
+        next();
+    }).catch(error=>{
+        console.log(error);
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Product.hasMany(User, {constraints:true, onDelete: 'CASCADE'});
 
-sequelize.sync().then().catch(err=>{
-    // console.log(err);
+User.hasMany(Product);
+Product.belongsTo(User);
+
+// {force:true}
+sequelize.sync()
+.then(result=>{
+    return User.findByPk(1);
+})
+.then(user=>{
+    if(!user){
+        console.log("hello")
+        return User.create({id: 1, name:'faizan', email:'test@test.com'});
+    }
+    return user;
+}).then(user=>{
+    // console.log(user);
+}).catch(err=>{
+    console.log(err);
 });
 app.listen(3000);
